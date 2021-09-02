40cdbcb7fc0ae6bd700d0a8d6b79d2780d183401
Matt McCortney <hazmatt101@gmail.com>
2021-09-02 13:22:10 -0400

    Add Dockerfile

--- run_as_admin.py
+++ run_as_admin.py
@@ -1,9 +1,5 @@
-import Flask
-
-
 from src.scripts.python import clean_build_game
 from src.scripts.python.mame_runner_util import MameRunnerUtil
-
 
 MameRunnerUtil.get_admin()
 clean_build_game.initialize()
